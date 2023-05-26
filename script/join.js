e7f475230cb5acbd09ce2fa1fdbdc7fed2dcaa76
JEJU-Ratus <smh1141@naver.com>
2023-05-26 20:22:44 +0900

    Feat : Fix 아이디 유효성 검사

--- script/join.js
+++ script/join.js
@@ -1,13 +1,15 @@
-// 참조
+// 인풋에 대한 참조
 const uid = document.getElementById('uid'),
 upw = document.getElementById('upw');
+// 유효성 검사 메시지에 대한 참조
+const uidMessage = document.getElementById('uid_message');
 // 정규표현식
 const uidReg = /^[a-z0-9]{1}[a-z0-9]{5,20}$/,
 upwReg = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^*])[\da-zA-Z!@#$%^*]{9,20}$/;
-
 // 폼 유효성 합격 기준 (합격 : true(기본값) / 불합격 : false)
 let formCheckArr = [];
 let finalFormCheck = true;
+// 유효성 검사 결과 메시지
 // onsubmit 속성함수로, 포커스 함수와 유효성 검사 함수 모두 합쳐서 작동시키는 메인 함수
 function joinFormCheck(){
     inputUid();
@@ -21,7 +23,8 @@ function joinFormCheck(){
             finalFormCheck = formCheckArr[i];
         }
     }
-    return finalFormCheck;
+    // return finalFormCheck;
+    return false;
 }
 // 포커스 함수
 function inputFocusAll(){
@@ -36,16 +39,24 @@ function inputFocusAll(){
 // 아이디 유효성 검사 함수
 function inputUid(){
     if(!uid.value){
-        console.log("값 없음");
+        uidMessage.textContent = "";
+        uidMessage.classList.add("error-message");
+        uidMessage.classList.remove("success-message");
+        uidMessage.textContent = "값이 입력되지 않았습니다.";
         formCheckArr[0] = false;
     }
     else{
         if(!uidReg.test(uid.value)){
-            console.log("유효성 검사 불합격");
-            formCheckArr[0] = false;
+            uidMessage.textContent = "";
+            uidMessage.classList.add("error-message");
+            uidMessage.classList.remove("success-message");
+            uidMessage.textContent = "값이 올바르게 입력되지 않았습니다.";
         }
         else{
-            console.log("유효성 검사 합격")
+            uidMessage.textContent = "";
+            uidMessage.classList.add("success-message");
+            uidMessage.classList.remove("error-message");
+            uidMessage.textContent = "완벽합니다!";
             formCheckArr[0] = true;
         }
     }
